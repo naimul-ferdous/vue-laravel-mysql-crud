92c727c8607f4096c351c5231c9a2c243a513f13
Naimul Ferdous <ferdousnaimul@gmail.com>
2022-08-08 16:22:28 +0600

    program model relationship

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -18,10 +18,35 @@ class Program extends Model
     }
 
     /**
-     * Get the teachers for the program.
+     * Get the events for the program.
      */
     public function events()
     {
         return $this->hasMany(Event::class);
+    }
+
+
+    /**
+     * Get the students for the program.
+     */
+    public function students()
+    {
+        return $this->hasMany(Student::class);
+    }
+
+    /**
+     * Get the feedbacks for the program.
+     */
+    public function feedbacks()
+    {
+        return $this->hasMany(Feedback::class);
+    }
+
+    /**
+     * Get the videos for the program.
+     */
+    public function videos()
+    {
+        return $this->hasMany(Youtube::class);
     }
 }
