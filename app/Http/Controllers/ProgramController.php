fd4755ddfd7876daa6bbefe599d059b7651b9b49
Naimul Ferdous <ferdousnaimul@gmail.com>
2022-08-08 16:58:52 +0600

    program controller

--- app/Http/Controllers/ProgramController.php
+++ app/Http/Controllers/ProgramController.php
@@ -3,18 +3,51 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Models\Program;
 
 
 class ProgramController extends Controller
 {
-    //program crud
-
+    //Program crud
     public function index()
     {
-        $program = Program::find(1);
-        $events = $program->events;
-        return $events;
+        $programs = Program::all()->toArray();
+        return array_reverse($programs);
+    }
+
+
+    public function store(Request $request)
+    {
+        $program = new Program([
+
+            'title' => $request->input('title'),
+            'channel' => $request->input('channel'),
+            'duration' => $request->input('duration'),
+            'description' => $request->input('description'),
+            'url' => $request->input('url'),
+        ]);
+        $program->save();
+        return response()->json('Program created!');
+    }
+
+    public function show($id)
+    {
+        $program = Program::find($id);
+        return response()->json($program);
+    }
+
+    public function update($id, Request $request)
+    {
+        $program = Program::find($id);
+        $program->update($request->all());
+        return response()->json('Program updated');
+    }
+
+    public function destroy($id)
+    {
+        $program = Program::find($id);
+        $program->delete();
+
+        return response()->json('Program deleted');
     }
 }
