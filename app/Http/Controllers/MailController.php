f66c1f782aeae9277b0f2ecc84dbaec87122d4e9
Naimul Ferdous <ferdousnaimul@gmail.com>
2022-08-13 18:29:57 +0600

    mail sending incomplete

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MailController extends Controller
+{
+    public function store(Request $request)
+    {
+
+
+        // 'title' => $request->input('title'),
+        // 'channel' => $request->input('channel'),
+        // 'duration' => $request->input('duration'),
+        // 'description' => $request->input('description'),
+        // 'url' => $request->input('url'),
+        // 'program_id' => $request->input('program_id'),
+
+
+
+
+        // $youtube->save();
+        // return response()->json('Youtube created!');
+    }
+}
